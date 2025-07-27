e6b5eebbd1e345ff9573daf0659ad92d1748e0b1
saten0 <linra7746@gmail.com>
2025-07-28 04:55:09 +0900

    Add pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
       <div className="container mx-auto w-full max-w-4xl p-8 bg-white rounded-2xl shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">皆川 諒のページです</h1>
         <p className="text-gray-600">
-          これは京都大学大学院で大学院生をしている皆川のページです。ページは作成途中です。Webページは初めて作るので、あまりちゃんとできていません。
+          これは京都大学大学院で大学院生をしている皆川のページです。ページは作成途中です。Webページは初めて作るので、あまりちゃんとできていません(Webは殆ど何もわからないので適当にNext.jsのテンプレを使ってみましたが、javascript自体触ったことないので相当に沼にハマっています)。
         </p>
-        <p>I'm Ryo Minakawa, a master student at Kyoto University. I'm interested in tensor networks and dynamical low-rank approximation.</p>
+        <p>I&apos;m Ryo Minakawa, a master student at Kyoto University. I&apos;m interested in tensor networks and dynamical low-rank approximation.</p>
       </div>
     </main>
   );
