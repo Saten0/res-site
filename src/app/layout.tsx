b39174adf90c9a7c03fffef1e02b6421ac67f4b7
saten0 <linra7746@gmail.com>
2025-07-28 04:30:51 +0900

    Add pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import './globals.css'
-
+import './globals.css';
+import Link from 'next/link';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,9 +7,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="min-h-screen flex flex-col">
         <header className="bg-gray-800 text-white p-4">
           <nav className="container mx-auto flex gap-4">
-            <a href="/"      className="hover:underline">Home</a>
-            <a href="/about" className="hover:underline">About</a>
-            <a href="/presentaiton" className="hover:underline">Presentation</a>
+            <Link href="/"      className="hover:underline">Home</Link>
+            <Link href="/about" className="hover:underline">About</Link>
+            <Link href="/presentaiton" className="hover:underline">Presentations</Link>
           </nav>
         </header>
         <main className="flex-1 container mx-auto p-6">{children}</main>
