ef2aab0e172fa116547cdf44e4d79a15930ba41e
saten0 <linra7746@gmail.com>
2025-07-28 04:41:11 +0900

    Add pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-blue-50">
-      <div className="p-8 bg-white rounded-2xl shadow-md text-center">
-        <h1 className="text-3xl font-bold mb-4">皆川のページです</h1>
-        <p className="text-gray-600">これは京都大学大学院で大学院生をしている皆川のページです。ページは作成途中です。Webページは初めて作るので、あまりちゃんとできていません。</p>
+    <main className="bg-blue-50 py-8">
+      <div className="container mx-auto w-full max-w-4xl p-8 bg-white rounded-2xl shadow-md text-center">
+        <h1 className="text-3xl font-bold mb-4">皆川 諒のページです</h1>
+        <p className="text-gray-600">
+          これは京都大学大学院で大学院生をしている皆川のページです。ページは作成途中です。Webページは初めて作るので、あまりちゃんとできていません。
+        </p>
+        <p>I'm Ryo Minakawa, a master student at Kyoto University. I'm interested in tensor networks and dynamical low-rank approximation.</p>
       </div>
     </main>
   );
